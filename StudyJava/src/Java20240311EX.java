eed24f25fe8730bfa4442b02bca4389b532b2444
pigking51 <145522570+pigking51@users.noreply.github.com>
2024-03-11 17:28:36 +0900

    240311최종

---
+++ StudyJava/src/Java20240311EX.java
@@ -0,0 +1,22 @@
+class A {
+    int m;
+    int n;
+    void init(int a, int b){
+        int c;
+        c = 3;
+        this.m = a;
+        this.n = b;
+    }
+    void work(){
+        this.init(2, 3);
+    }
+}
+public class Java20240311EX {
+    public static void main(String[] args) {
+        A a = new A();
+        a.work();
+        System.out.println(a.m);
+        System.out.println(a.n);
+    }
+
+}
