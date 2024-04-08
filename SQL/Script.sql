e929585e9b3bef1b05a606d5b97ecb2379bcf51e
pigking51 <145522570+pigking51@users.noreply.github.com>
2024-04-08 17:29:46 +0900

    240408SQL

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -13,12 +13,12 @@ select 이름, 직위 from 사원
 -- 문제 3
 
 select 고객회사명, 담당자명 from 고객
- where 고객번호 = 'CCOPI';
+	where 고객번호 = 'CCOPI';
 
 -- 문제 4
 
 select 주문번호, 주문일 from 주문
- 	where 주문일 > 2021-05-01
+ 	where 주문일 > '2021-05-01'
  	order by 주문일 desc;
 
 -- 문제 5
@@ -37,5 +37,52 @@ select 주문번호, 주문일 from 주문
  select 고객회사명, 마일리지 from 고객
  	where 마일리지 >= 1000
  	order by 마일리지 desc;
+ 
+ -- 문제 8
+ 
+ select 주문번호, 주문수량 from 주문세부
+ 	where 제품번호 = 77;
+ 
+ -- 문제 9
+ 
+ select 주문번호, 주문일 from 주문
+ 	where 주문일 < 210101;
+ 	-- where 주문일 < '2021-01-01';
+ 
+ -- 문제 10
+ 
+ select 이름, 성별, 직위 from 사원
+ 	-- where 성별 = '여' and  직위 = '사원';
+ 	where 성별 = '여' &&  직위 = '사원';
+ 
+ -- 문제 11
+ 
+ select * from 제품
+ 	where 단가 >= 8000
+ 	order by 제품명;
+ 
+ -- 문제 12
+ 
+ select 주문번호, 주문수량 from 주문세부
+ 	where 주문수량 >= 5
+ 	order by 주문수량 desc;
+ 
+ -- 문제 13
+ 
+ select 이름, 생일 from 사원
+ 	-- where 생일 > 900101;
+ 	where 생일 > '1990-01-01';
+ 
+ -- 문제 14
+ 
+ select 이름, 입사일 from 사원
+ 	order by 입사일 desc
+ 	limit 1;
+ 
+ -- 문제 15
+ 
+ select 고객번호, 고객회사명 from 고객
+ 	where 도시 = '대전광역시';
+ 
 
 
